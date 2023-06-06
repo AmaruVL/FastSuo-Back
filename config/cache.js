2b88ca06daab32dcd22a504ef1be6aca2660af64
AmaruVL <182940@unsaac.edu.pe>
2023-06-06 05:05:39 -0500

    variable cache agregado

---
+++ config/cache.js
@@ -0,0 +1,25 @@
+require('dotenv').config() // this is important!
+
+const setValue = (key, value, lifetime = 43200) => {
+  //lifetime in seconds, 12h (43200s) by default
+  const expires = Date.now() + lifetime * 1000
+  const cache = JSON.parse(process.env.CACHE)
+  process.env['CACHE'] = JSON.stringify({ ...cache, [key]: { value, expires } })
+}
+
+const getValue = (key) => {
+  const cache = JSON.parse(process.env.CACHE)
+  const item = cache[key]
+  if (!item) return null
+  if (item.expires <= Date.now()) {
+    delete cache[key]
+    process.env['CACHE'] = JSON.stringify({ ...cache })
+    return null
+  }
+  return item.value
+}
+
+module.exports = {
+  setValue,
+  getValue,
+}
