3e60f6c977551ce450c0d3f926885109b3b7229c
AmaruVL <182940@unsaac.edu.pe>
2023-06-09 14:26:28 -0500

    Reportes eliminados de rutas

--- rutas/Menu/Reportes/Resumen_Saldos/index.js
+++
@@ -1,9 +0,0 @@
-// const rutas_reportes = require("express").Router();
-
-// const reportes = require("../../../../controllers/reportes");
-
-// rutas_reportes.get("/", reportes.resumen_saldos);
-// rutas_reportes.get("/saldos2/:opcion",reportes.resumen_saldos2);
-// rutas_reportes.get("/resumenpdf/:opcion",reportes.listaresumensaldos);
-
-// module.exports = rutas_reportes;
