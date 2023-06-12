e032b15842888c5a36f3f2146a060fb8b4f6fc6f
Denis Omar Cuyo Ttito <182906@unsaac.edu.pe>
2023-06-12 16:02:19 -0500

    Endpoints vechículos

---
+++ controllers/vehiculo.js
@@ -0,0 +1,121 @@
+const Sequalize = require("sequelize");
+const models = require("../models");
+
+var filename = module.filename.split("/").slice(-1);
+
+exports.listar = (req, res) => {
+  var logger = req.app.get("winston");
+  const token = req.header("Authorization").split(" ")[1];
+
+  models.vehiculo
+    .findAll({
+      order: [["placa", "ASC"]],
+      attributes: ["id_vehiculo", "placa", "marca", "clase", "modelo", "color"],
+    })
+    .then(lista => {
+      res.json(lista);
+    })
+    .catch(err => {
+      logger.log("error", { ubicacion: filename, token: token, message: err.message });
+      res.json({
+        error: err.errors,
+      });
+    });
+};
+
+exports.buscar = (req, res) => {
+  var logger = req.app.get("winston");
+  const token = req.header("Authorization").split(" ")[1];
+
+  models.vehiculo
+    .findByPk(req.params.id_vehiculo)
+    .then(objeto => {
+      res.json(objeto);
+    })
+    .catch(err => {
+      logger.log("error", { ubicacion: filename, token: token, message: err.message });
+      res.status(400).json({
+        error: err.errors,
+      });
+    });
+};
+
+exports.crear = (req, res) => {
+  var logger = req.app.get("winston");
+  const token = req.header("Authorization").split(" ")[1];
+
+  models.vehiculo
+    .create({
+      placa: req.body.placa,
+      marca: req.body.marca,
+      clase: req.body.clase,
+      modelo: req.body.modelo,
+      color: req.body.color,
+    })
+    .then(objeto => {
+      res.json({
+        mensaje: objeto,
+      });
+    })
+    .catch(err => {
+      logger.log("error", { ubicacion: filename, token: token, message: err.message });
+      res.status(400).json({
+        error: err.errors,
+      });
+    });
+};
+
+exports.actualizar = (req, res) => {
+  var logger = req.app.get("winston");
+  const token = req.header("Authorization").split(" ")[1];
+
+  models.vehiculo
+    .update(
+      {
+        placa: req.body.placa,
+        marca: req.body.marca,
+        clase: req.body.clase,
+        modelo: req.body.modelo,
+        color: req.body.color,
+      },
+      {
+        where: {
+          id_vehiculo: req.params.id_vehiculo,
+        },
+      },
+    )
+    .then(objeto => {
+      res.json({
+        mensaje: "Actualizado",
+      });
+    })
+    .catch(err => {
+      logger.log("error", { ubicacion: filename, token: token, message: err.message });
+      res.status(400).json({
+        error: err.errors,
+      });
+    });
+};
+
+exports.eliminar = (req, res) => {
+  var logger = req.app.get("winston");
+  const token = req.header("Authorization").split(" ")[1];
+
+  models.vehiculo
+    .destroy({
+      where: {
+        id_vehiculo: req.params.id_vehiculo,
+      },
+    })
+    .then(objeto => {
+      res.json({
+        mensaje: "Eliminado",
+      });
+    })
+    .catch(err => {
+      logger.log("error", { ubicacion: filename, token: token, message: err.message });
+      res.status(400).json({
+        error: err.errors,
+      });
+    });
+};
